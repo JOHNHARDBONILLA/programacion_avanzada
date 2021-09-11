40393efb5a291a1a19c12a33dc893ad85a60b180
JOHNHARDBONILLA <65375779+JOHNHARDBONILLA@users.noreply.github.com>
2021-09-10 21:55:02 -0500

    Add files via upload

---
+++ estRepWhile2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estRepWhile2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,x;
+            string lnea;
+            Console.Write("ingrese el valor final: ");
+            lnea = Console.ReadLine();
+            n = int.Parse(lnea);
+            x = 1;
+            while (x<=n)
+            {
+                Console.Write(x);
+                Console.Write(" - ");
+                x = x+1;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
