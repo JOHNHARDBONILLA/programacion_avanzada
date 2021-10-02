1f8f14f3662969935d0bf78917e6f8864dcd6ce8
JOHNHARDBONILLA <65375779+JOHNHARDBONILLA@users.noreply.github.com>
2021-10-01 21:37:51 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace proyecto14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnSaludo = new System.Windows.Forms.Button();
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.btnDespedida = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSaludo
+            // 
+            this.btnSaludo.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnSaludo.Location = new System.Drawing.Point(521, 104);
+            this.btnSaludo.Name = "btnSaludo";
+            this.btnSaludo.Size = new System.Drawing.Size(97, 38);
+            this.btnSaludo.TabIndex = 0;
+            this.btnSaludo.Text = "saludo";
+            this.btnSaludo.UseVisualStyleBackColor = false;
+            this.btnSaludo.Click += new System.EventHandler(this.btnSaludo_Click);
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Location = new System.Drawing.Point(157, 117);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(35, 13);
+            this.lblMensaje.TabIndex = 1;
+            this.lblMensaje.Text = "label1";
+            // 
+            // btnDespedida
+            // 
+            this.btnDespedida.Location = new System.Drawing.Point(521, 182);
+            this.btnDespedida.Name = "btnDespedida";
+            this.btnDespedida.Size = new System.Drawing.Size(97, 41);
+            this.btnDespedida.TabIndex = 2;
+            this.btnDespedida.Text = "Despedida";
+            this.btnDespedida.UseVisualStyleBackColor = true;
+            this.btnDespedida.Click += new System.EventHandler(this.btnDespedida_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDespedida);
+            this.Controls.Add(this.lblMensaje);
+            this.Controls.Add(this.btnSaludo);
+            this.Name = "Form1";
+            this.Text = "principal";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnSaludo;
+        private System.Windows.Forms.Label lblMensaje;
+        private System.Windows.Forms.Button btnDespedida;
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,45 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace EstructurasCondicionales
+namespace proyecto14
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            int num1,num2;
-            string linea;
-            Console.Write("ingrese un valor: ");
-            linea = Console.ReadLine();
-            num1 = int.Parse(linea);
-            Console.Write("ingere un segundo valor");
-            linea = Console.ReadLine();
-            num2 = int.Parse(linea);
-            if(num1>num2)
-            {
-                int suma, diferencia;
-                suma = num1 + num2;
-                diferencia = num1 - num2;
-                Console.Write("la suma de los valores es: ");
-                Console.WriteLine(suma);
-                Console.Write("la diferencia de los valores es: ");
-                Console.WriteLine(diferencia);
-
-            }
-            else
-            {
-                int producto, division;
-                producto = num1 * num2;
-                division = num1 / num2;
-                Console.Write("el producto de los valores es: ");
-                Console.WriteLine(producto);
-                Console.Write("la division de los valores es: ");
-                Console.WriteLine(division);
-            }
-            Console.ReadKey();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
